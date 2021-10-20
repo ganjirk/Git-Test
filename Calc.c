29d3f5a00396915e5dd171f54985edf5cd64eb44
Raj Kumar <rajkumar303@gmail.com>
2021-10-20 22:38:41 +0530

    updates

--- Calc.c
+++ Calc.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
-#include "Include/basichead.h"
+#include "basichead.h"
 #include <stdlib.h>
 int calc ()
 {
   int c, a, b;
+  float r;
   printf
     ("Select Option: \n\n 1. Add \n 2. Multiply\n 3. Substract\n 4. Division\n\n Enter Choice: ");
   scanf ("%d", &c);
   if (c > 4)
   {
-      printf ("Wrong Selection - Bye\n");
-      exit;
+    printf ("\nWrong Selection - Bye\n");
+    exit;
   }
-  else if (c < 5)
+  else
   {
-      printf ("Enter First number: ");
+      printf ("\nEnter First number: ");
       scanf ("%d", &a);
       printf ("Enter Second number: ");
-      scanf ("%d", &b);      
-  }
-  if (c == 1)
-  {
-    add (a, b);
-  }
-  else if (c == 2)
-  {
-    multiply (a, b);
-  }
-  else if (c == 3)
-  {
-      substract (a, b);
-  }
-  else if (c == 4)
-  {
-    division (a, b);
-  }
-  return 0;
+      scanf ("%d", &b);  
+      switch (c)
+      {
+          case 1:
+            add(a,b);
+            break;  
+          case 2:
+            multiply(a,b);
+            break; 
+          case 3:
+            substract(a,b);
+            break; 
+          case 4:
+            division(a,b);
+            break; 
+          default:
+            printf ("\nWrong Selection - Bye\n");
+      }
+    return 0;
+    }
 }
