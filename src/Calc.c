3d8ac91c770f59a358e0ae82c75547ade14035fa
Raj Kumar <rajkumar303@gmail.com>
2021-11-06 01:07:45 +0530

    updates

--- src/Calc.c
+++ src/Calc.c
@@ -5,15 +5,10 @@ int calc ()
 {
   int c, a, b;
   float r;
-  printf
-    ("Select Option: \n\n 1. Add \n 2. Multiply\n 3. Substract\n 4. Division\n 5. concatenate\n\n Enter Choice: ");
+printf
+    ("Select Option: \n\n 1. Add \n 2. Multiply\n 3. Substract\n 4. Division\n 5. concatenate\n 6. Convert to Binary \n\n Enter Choice: ");
   scanf ("%d", &c);
-  if (c > 4)
-  {
-    printf ("\nWrong Selection - Bye\n");
-    exit;
-  }
-  else
+ if (c <6)
   {
       printf ("\nEnter First number: ");
       scanf ("%d", &a);
@@ -33,12 +28,26 @@ int calc ()
           case 4:
             division(a,b);
             break; 
-		  case 5:
-            concatenate (a,b);	
-			break; 
+          case 5:
+            concatenate (a,b);
+            break; 
+          //case 6:
+          //ConvertBinary (a);
+          //  break; 
           default:
             printf ("\nWrong Selection - Bye\n");
       }
     return 0;
+    }
+	else
+	{
+	  printf ("\nEnter Any Decimal number: ");
+      scanf ("%d", &a);
+      switch (c)
+      {
+	  case 6:
+          ConvertBinary (a);
+          break; 
+      }
     }
 }
